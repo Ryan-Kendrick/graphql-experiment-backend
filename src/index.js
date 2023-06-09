7b62610434bfdb66bebcc1c1f8b988d18a575547
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-06-10 11:56:46 +1200

    add prisma client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client')
 const fs = require('fs')
 const path = require('path')
 
@@ -36,10 +37,14 @@ const resolvers = {
   },
 }
 
-// 3
+const prisma = new PrismaClient()
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
+  context: {
+    prisma,
+  },
 })
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
