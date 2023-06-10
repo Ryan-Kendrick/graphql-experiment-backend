3f82bc724d5e29888dd4327fa6a615edb851559e
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-06-10 12:02:46 +1200

    remove hardcoded data

--- src/index.js
+++ src/index.js
@@ -3,36 +3,27 @@ const { PrismaClient } = require('@prisma/client')
 const fs = require('fs')
 const path = require('path')
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQl',
-  },
-]
-
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
 
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prims.link.findMany()
+    },
     link: (parent, args) => {
       const data = links.find((link) => link.id === args.id)
       return data
     },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
     },
   },
 }
